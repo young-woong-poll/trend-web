6e2c26f2bf74932631f65563cf7c573ce6569782
woongs <wil953742@gmail.com>
2025-11-16 11:26:32 +0900

    ✨ 메인화면 작업

--- src/components/features/Main/MainView.tsx
+++ src/components/features/Main/MainView.tsx
@@ -8,40 +8,30 @@ import { FlexibleLayout } from '@/components/common/FlexibleLayout/FlexibleLayou
 import { Header } from '@/components/common/Header/Header';
 import styles from '@/components/features/Main/MainView.module.scss';
 import { PollCard } from '@/components/features/Main/PollCard/PollCard';
+import { useMainDisplay } from '@/hooks/api';
 
 export const MainView: FC = () => {
   const router = useRouter();
 
+  const { data } = useMainDisplay();
+
   const handleStartSurvey = (pollId: string) => router.push(`/vote/${pollId}`);
+
   return (
     <>
       <Header />
       <FlexibleLayout>
         <div className={styles.container}>
-          <PollCard
-            title="대한민국 연애 난제"
-            subtitle="당신의 문제라면?"
-            participantCount={1200}
-            onStart={() => handleStartSurvey('love')}
-          />
-          <PollCard
-            title="대한민국 연애 난제"
-            subtitle="당신의 문제라면?"
-            participantCount={1200}
-            onStart={() => handleStartSurvey('love')}
-          />
-          <PollCard
-            title="대한민국 연애 난제"
-            subtitle="당신의 문제라면?"
-            participantCount={1200}
-            onStart={() => handleStartSurvey('love')}
-          />
-          <PollCard
-            title="대한민국 연애 난제"
-            subtitle="당신의 문제라면?"
-            participantCount={1200}
-            onStart={() => handleStartSurvey('love')}
-          />
+          {data?.trends.map((trend) => (
+            <PollCard
+              key={trend.id}
+              title={trend.title}
+              subtitle={trend.label}
+              imageUrl={trend.imageUrl}
+              participantCount={trend.participantsCount}
+              onStart={() => handleStartSurvey(trend.id)}
+            />
+          ))}
         </div>
       </FlexibleLayout>
     </>
