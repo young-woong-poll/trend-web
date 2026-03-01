6fa7f7793d572e182cd91037323fa9bf32694166
woongs <wil953742@gmail.com>
2026-03-01 19:25:35 +0900

    타입 에러 해결

---
+++ src/types/assets.d.ts
@@ -0,0 +1,26 @@
+import type { StaticImageData } from 'next/image';
+
+declare module '*.png' {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: StaticImageData;
+  export default content;
+}
