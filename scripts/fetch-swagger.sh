3964b75b9d2b6fe2fcda1cbc25d512344f14141c
woongs <wil953742@gmail.com>
2026-03-30 22:06:00 +0900

    각종 문서 업데이트

--- scripts/fetch-swagger.sh
+++ scripts/fetch-swagger.sh
@@ -7,5 +7,12 @@ API_URL="https://beta-hotpick-api.votebox.kr/v3/api-docs"
 OUTPUT="swagger.json"
 
 echo "Fetching swagger.json from $API_URL ..."
-curl -sf "$API_URL" | jq '.' > "$OUTPUT"
+curl -sf "$API_URL" | jq '
+  # http 타입 securityScheme에서 유효하지 않은 "name" 필드 제거 (OpenAPI 스펙 준수)
+  if .components.securitySchemes then
+    .components.securitySchemes |= with_entries(
+      if .value.type == "http" then .value |= del(.name) else . end
+    )
+  else . end
+' > "$OUTPUT"
 echo "Updated $OUTPUT"
