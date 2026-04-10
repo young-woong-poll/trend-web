52c9c4c7d2c86c506079e4e8a13c12bce0ee16fa
woongs <wil953742@gmail.com>
2026-04-10 23:34:47 +0900

    머지하다 에러?

--- src/generated/api/client/auth-controller/auth-controller.ts
+++ src/generated/api/client/auth-controller/auth-controller.ts
@@ -10,7 +10,6 @@ import type {
   BaseResponseSignupResponse,
   BaseResponseUserResponse,
   BaseResponseVoid,
-  CheckNicknameParams,
   KakaoLoginRequest,
   SignupRequest,
   UpdateProfileRequest,
@@ -18,6 +17,7 @@ import type {
 
 import { customInstance } from '../../../../lib/axios-mutator';
 import type { BodyType } from '../../../../lib/axios-mutator';
+import { CheckNicknameParams } from '../../../models/checkNicknameParams';
 
 type SecondParameter<T extends (...args: never) => unknown> = Parameters<T>[1];
 
