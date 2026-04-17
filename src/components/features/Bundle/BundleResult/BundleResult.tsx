f3275e3d9f0bf04032fac0f74b38e78347922baa
woongs <wil953742@gmail.com>
2026-04-17 23:42:00 +0900

    번들 결과 버튼 크기 줄이기

--- src/components/features/Bundle/BundleResult/BundleResult.tsx
+++ src/components/features/Bundle/BundleResult/BundleResult.tsx
@@ -304,7 +304,7 @@ export const BundleResult: FC<BundleResultProps> = ({ slug }) => {
             className={styles.ctaOneToOne}
             onClick={() => setShowCompareModal(true)}
           >
-            다른친구랑 케미 보기
+            다른 친구랑 케미 보기
           </button>
           <button type="button" className={styles.ctaGroup} onClick={() => setShowGroupModal(true)}>
             그룹 케미 보기
